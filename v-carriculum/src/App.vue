acb2127dc1aa67719ef233ab99c4ebbf17cab774
tony-tony-alpha <shuya.inada@gmail.com>
2025-01-18 18:47:21 +0900

    add

--- v-carriculum/src/App.vue
+++ v-carriculum/src/App.vue
@@ -1,30 +1,28 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h1>{{ title }}</h1>
+    <p>{{ description }}</p>
+    <TaskItem :title="title" @update:title="updateTitle" />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
+<script setup lang="ts">
+import { ref } from 'vue';
+import TaskItem from './components/TaskItem.vue';
+const title = ref('Vue Curriculum');
+const description = ref('Vue Curriculum Description');
+
+const updateTitle = (value: string) => {
+  title.value = value;
+};
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+h1 {
+  color: red;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+p {
+  color: blue;
 }
 </style>
---
+++ v-carriculum/src/components/TaskItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <p>{{ props.title }}</p>
+    <button @click="updateTitle('TaskItem')">Update</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps, defineEmits } from 'vue';
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+});
+
+const emit = defineEmits<{
+  (e: 'update:title', value: string): void;
+}>();
+
+const updateTitle = (value: string) => {
+  emit('update:title', value);
+};
+
+</script>
+
+<style scoped>
+</style>
