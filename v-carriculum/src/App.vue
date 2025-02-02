b3847d6365672f896fc412430c82bd8fbe7ec2b1
tony-tony-alpha <shuya.inada@gmail.com>
2025-02-02 21:31:51 +0900

    complete-1

--- v-carriculum/src/App.vue
+++ v-carriculum/src/App.vue
@@ -7,13 +7,28 @@
   </header>
 
   <main>
-    <router-view />
+    <div class="main-layout">
+      <Sidebar></Sidebar>
+      <div class="content">
+        <router-view />
+      </div>
+    </div>
   </main>
 </template>
-  
+
 <script setup lang="ts">
 import { RouterView, RouterLink } from 'vue-router';
+import Sidebar from './pages/SideBar.vue';
 </script>
 
 <style scoped>
+.main-layout {
+  display: flex;
+  width: 100%;
+}
+
+.content {
+  flex: 1;
+  padding: 20px;
+}
 </style>
---
+++ v-carriculum/src/pages/SideBar.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="sidebar">
+        <h1>My App</h1>
+        <nav>
+            <router-link to="/todo">Todo</router-link>
+            <router-link to="/login">Login</router-link>
+        </nav>
+    </div>
+</template>
+
+<script setup lang="ts">
+// import { useUserStore } from '../store/useUserStore';
+
+// const userStore = useUserStore()
+
+
+</script>
+
+<style scoped>
+.sidebar {
+    width: 200px;
+    padding: 20px;
+    background-color: #f2f2f2;
+}
+
+.sidebar nav {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+}
+
+.sidebar nav a {
+    text-decoration: none;
+    color: #4b3a8f;
+    font-size: 18px;
+    font-weight: bold;
+}
+
+</style>
